a90fa78ebbdc7f9d5a6f31a42d7edaa44d0c9a05
SizableShrimp <sizableshrimp@sizableshrimp.me>
2021-07-18 22:05:14 -0500

    Add module merging
    Bump SJH to 0.9.44

--- src/main/java/cpw/mods/bootstraplauncher/BootstrapLauncher.java
+++ src/main/java/cpw/mods/bootstraplauncher/BootstrapLauncher.java
@@ -3,6 +3,7 @@ package cpw.mods.bootstraplauncher;
 import cpw.mods.cl.JarModuleFinder;
 import cpw.mods.cl.ModuleClassLoader;
 import cpw.mods.jarhandling.SecureJar;
+import cpw.mods.jarhandling.impl.SimpleJarMetadata;
 
 import java.io.File;
 import java.lang.module.ModuleFinder;
@@ -10,6 +11,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
@@ -17,13 +19,14 @@ import java.util.ServiceLoader;
 import java.util.Set;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class BootstrapLauncher {
     private static final boolean DEBUG = System.getProperties().containsKey("bsl.debug");
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        var legacyCP = Objects.requireNonNull(System.getProperty("legacyClassPath"), "Missing legacyClassPath, cannot bootstrap");
+        var legacyCP = Objects.requireNonNull(System.getProperty("legacyClassPath", System.getProperty("java.class.path")), "Missing legacyClassPath, cannot bootstrap");
         var ignoreList = System.getProperty("ignoreList", "/org/ow2/asm/,securejarhandler"); //TODO: find existing modules automatically instead of taking in an ignore list.
         var ignores = ignoreList.split(",");
 
@@ -31,7 +34,7 @@ public class BootstrapLauncher {
         var jars = new ArrayList<>();
 
         outer:
-        for  (var legacy : legacyCP.split(File.pathSeparator)) {
+        for (var legacy : legacyCP.split(File.pathSeparator)) {
             for (var filter : ignores) {
                 if (legacy.contains(filter)) {
                     if (DEBUG)
@@ -42,7 +45,7 @@ public class BootstrapLauncher {
 
             var path = Paths.get(legacy);
             if (DEBUG)
-                System.out.println(path.toString());
+                System.out.println(path);
             var jar = SecureJar.from(new PkgTracker(Set.copyOf(previousPkgs), path), path);
             var pkgs = jar.getPackages();
             if (DEBUG)
@@ -50,7 +53,7 @@ public class BootstrapLauncher {
             previousPkgs.addAll(pkgs);
             jars.add(jar);
         }
-        var finder = jars.toArray(SecureJar[]::new);
+        var finder = mergeModules(jars.toArray(SecureJar[]::new));
 
         var alltargets = Arrays.stream(finder).map(SecureJar::name).toList();
         var jf = JarModuleFinder.of(finder);
@@ -63,6 +66,27 @@ public class BootstrapLauncher {
         final var loader = ServiceLoader.load(layer.layer(), Consumer.class);
         // This *should* find the service exposed by ModLauncher's BootstrapLaunchConsumer {This doc is here to help find that class next time we go looking}
         ((Consumer<String[]>)loader.stream().findFirst().orElseThrow().get()).accept(args);
+    }
+
+    private static SecureJar[] mergeModules(SecureJar[] finder) {
+        // newModule=module1,module2;otherModule=module3,module4
+        var mergeModules = System.getProperty("mergeModules");
+        if (mergeModules == null)
+            return finder;
+
+        var moduleMap = Arrays.stream(finder).collect(Collectors.toMap(SecureJar::name, Function.identity()));
+        for (var merge : mergeModules.split(";")) {
+            var split = merge.split("=");
+            var key = split[0];
+            var targets = Arrays.stream(split[1].split(",")).map(moduleMap::remove).filter(Objects::nonNull).toList();
+            var filter = targets.stream().map(SecureJar::getPathFilter).reduce(BiPredicate::or).orElseThrow();
+            var paths = targets.stream().map(SecureJar::getPrimaryPath).toArray(Path[]::new);
+
+            var newJar = SecureJar.from(sj -> new SimpleJarMetadata(key, null, sj.getPackages(), sj.getProviders()), filter, paths);
+            moduleMap.put(key, newJar);
+        }
+
+        return moduleMap.values().toArray(SecureJar[]::new);
     }
 
     private record PkgTracker(Set<String> packages, Path paths) implements BiPredicate<String, String> {
